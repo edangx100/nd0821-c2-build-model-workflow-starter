97650ff8521ed56cd96ef5634f97551179f83adb
Ed <edangx20@gmail.com>
2022-01-21 23:22:43 -0800

    Data Cleaning Step update

--- main.py
+++ main.py
@@ -38,21 +38,30 @@ def go(config: DictConfig):
         if "download" in active_steps:
             # Download file and load in W&B
             _ = mlflow.run(
-                f"{config['main']['components_repository']}/get_data",
-                "main",
-                parameters={
-                    "sample": config["etl"]["sample"],
-                    "artifact_name": "sample.csv",
-                    "artifact_type": "raw_data",
-                    "artifact_description": "Raw file as downloaded"
-                },
-            )
+                    f"{config['main']['components_repository']}/get_data",
+                    "main",
+                    parameters={
+                        "sample": config["etl"]["sample"],
+                        "artifact_name": "sample.csv",
+                        "artifact_type": "raw_data",
+                        "artifact_description": "Raw file as downloaded"
+                    },
+                )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+                    os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+                    "main",
+                    parameters={
+                        "tmp_directory": tmp_dir,
+                        "input_artifact": "sample.csv:latest",
+                        "output_artifact": "clean_sample.csv",
+                        "output_type": "clean_sample",
+                        "output_description": "Data with outliers and null values removed",
+                        "min_price": config['etl']['min_price'],
+                        "max_price": config['etl']['max_price']
+                    },
+                )
 
         if "data_check" in active_steps:
             ##################
